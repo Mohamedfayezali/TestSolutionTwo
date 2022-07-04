4535923f927f61d812714756a80422f2899954e8
Mohamed Foad <MohamedFoad@FortTeck.com>
2022-07-04 17:09:53 +0200

    Adding Testing Projects to the solution

---
+++ TestSolution.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestSolution.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TestSolutoin.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TestSolutoin.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
